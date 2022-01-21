3ef2bd87500357e799b79ff362704950f7ac62e2
Carlos Castillo <closcastillo95@gmail.com>
2022-01-20 22:07:11 -0800

    force https redirect

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use((req, res, next) => {
+    if (req.get('X-Forwarded-Proto') === 'http') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+  
+    next();
+});
 
 app.use(express.static('./dist'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '.dist/index.html'));
 })
-
-// app.get('/dist/main.js', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './dist/main.js'));
-// })
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:" + PORT);
